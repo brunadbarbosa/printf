2a2df60d803264ae1424bd28cb9699c52a11380f
Bruna Maria Duarte Barbosa <brmaria-@c2r1s9.42porto.com>
2025-05-10 09:27:25 +0100

    Setting up the conditions and creating every function necessary to handle what it needs to be handled

--- ft_printf.c
+++ ft_printf.c
@@ -6,22 +6,88 @@
 /*   By: brmaria- <brmaria-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 18:33:55 by brmaria-          #+#    #+#             */
-/*   Updated: 2025/05/08 17:33:45 by brmaria-         ###   ########.fr       */
+/*   Updated: 2025/05/09 18:11:18 by brmaria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <unistd.h>
 
-void    ft_printchar(char c, int *count)
+int    ft_putchar(char c)
 {
     write(1, &c, 1);
-    *count += 1;
+    return (1);
 }
 
-ft_check (char format, va_list args, int *count)
+int	ft_putnbr(long int n)
 {
-    
+    int i;
+
+    i = 0;
+	if (n == -2147483648)
+		return (ft_putstr("-2147483648"));
+	if (n < 0)
+	{
+		i = i + ft_putchar('-');
+		n = n * (-1);
+	}
+	if (n > 9)
+		i = i + ft_putnbr(n / 10);
+	i = i + ft_putchar((n % 10) + 48);
+	return (i);
+}
+
+int	ft_putstr(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		ft_putchar(s[i]);
+		i++;
+	}
+    return (i);
+}
+
+static int  ft_check(char format, va_list args, int *count)
+{  
+    if (format == '%')
+       return (ft_putchar('%'));
+    else if (format == 'd' || format == 's')
+    {
+        *count++;
+        return (ft_putnbr(va_arg(args, int)));
+    }
+    else if(format == 'u')
+    {
+        *count++;
+        return (ft_putnbr(va_arg(args, unsigned int)));
+    }
+    else if(format == 'x')
+    {
+        *count++;
+        return (ft_puthex(va_arg(args, unsigned int), 0));
+    }
+    else if(format == 'X')
+    {
+        *count++;
+        return (ft_puthex(va_arg(args, unsigned int), 1));
+    } 
+    else if (format == 's')
+    {
+        *count++;
+        return (ft_putstr(va_arg(args, char *)));
+    }
+    else if (format == 'c')
+        return (ft_putchar(va_arg(args, char)));
+    else if(format == 'p')
+    {
+        count++;
+        return (ft_putptr(va_arg(args, void *)));
+    }
+    else
+        return (0);
 }
 
 ft_validate(char format)
@@ -42,7 +108,7 @@ ft_validate(char format)
 
 int ft_printf(const char *format, ...)
 {
-    va_list args;
+    va_list args; 
     int     i;
     int     count;
 
@@ -56,12 +122,13 @@ int ft_printf(const char *format, ...)
         if(format[i] == '%' && ft_validate(format[i + 1]))
         {
             i++;
-            ft_printchar(ft_check(format[i], args, &count), &count);
+            ft_check(format[i], args, &count);
         }
         else
             ft_printchar(format[i], &count);
         i++;
     }
+    va_end(args);
     return (count);
 }
 
